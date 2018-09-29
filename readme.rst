43fc91ab13680624cd400cc032972fda8c174023
Gannicus <513720453@qq.com>
2018-09-29 11:21:02 +0800

    新增readme文件以及setup

---
+++ readme.rst
@@ -0,0 +1,21 @@
+==========
+ScrapydArt
+==========
+
+.. image:: https://secure.travis-ci.org/scrapy/scrapyd.svg?branch=master
+    :target: http://travis-ci.org/scrapy/scrapyd
+
+.. image:: https://codecov.io/gh/scrapy/scrapyd/branch/master/graph/badge.svg
+    :target: https://codecov.io/gh/scrapy/scrapyd
+
+ScrapydArt base on Scrapyd, and added auth/filter/order……
+
+ScrapydArt is a service for running `Scrapy`_ spiders.
+
+It allows you to deploy your Scrapy projects and control their spiders using an
+HTTP JSON API.
+
+The documentation (including installation and usage) can be found at:
+http://scrapydart.readthedocs.org/
+
+.. _Scrapy: https://github.com/dequinns/scrapydart
---
+++ setup.py
@@ -0,0 +1,58 @@
+import sys
+
+try:
+    from setuptools import setup
+    using_setuptools = True
+except ImportError:
+    from distutils.core import setup
+    using_setuptools = False
+
+from os.path import join, dirname
+
+with open(join(dirname(__file__), 'scrapyd/VERSION')) as f:
+    version = f.read().strip()
+
+setup_args = {
+    'name': 'scrapydArt',
+    'version': version,
+    'url': 'https://github.com/dequinns/scrapydart',
+    'description': 'Scrapyd Plus, auth, order, filter……',
+    'long_description': open('README.rst').read(),
+    'author': 'quinns',
+    'maintainer': 'quinns',
+    'maintainer_email': 'quinns@aliyun.com',
+    'license': 'BSD',
+    'packages': ['scrapyd'],
+    'include_package_data': True,
+    'zip_safe': False,
+    'classifiers': [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: No Input/Output (Daemon)',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+}
+
+
+if using_setuptools:
+    setup_args['install_requires'] = [
+        'Twisted>=8.0',
+        'Scrapy>=1.0',
+        'six',
+        'enum-compat',
+    ]
+    setup_args['entry_points'] = {'console_scripts': [
+        'scrapyd = scrapyd.scripts.scrapyd_run:main'
+    ]}
+else:
+    setup_args['scripts'] = ['scrapyd/scripts/scrapyd_run.py']
+
+setup(**setup_args)
